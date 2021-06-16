f8ee282cc82b20a91ed69508331ba9d6839d365d
anatsrii <anat.allsolution@gmail.comgit config user.name anatsriigit config user.email git config --global user.email anat.allsolution@gmail.com>
2021-06-16 23:39:08 +0700

    Update python exercises

---
+++ 2.1Problem/PrintString.py
@@ -0,0 +1,61 @@
+#2.1.1
+print("Hello")
+
+#2.1.2
+print("Hello World")
+
+#2.1.3
+print("hello_python")
+
+#2.1.4
+print("Merry X'Mas")
+
+#2.1.5
+print("I want to ask you \"Why don\'t you drive to work?\"")
+
+#2.1.6
+print("\"I don\'t have a car\"")
+
+#2.1.7
+print("You got a new job !? That\'s so exciting")
+
+#2.1.8
+print('สวัสดีวันจันทร์')
+
+#2.1.9
+print('ความแตกต่างระหว่างคนเก่งกับไม่เก่ง คือ "การใช้เวลาว่างให้เป็นประโยชน์"')
+
+#2.1.10
+print('/\\/\\/\\')
+
+#2.1.11
+print('''a
+an
+ant''')
+
+#2.1.12
+print('''
+\t*
+*\t*\t*
+\t*
+''')
+
+#2.1.13
+print('''
+*\t+\t*
++\t*\t+
+*\t+\t*
+''')
+
+#2.1.14
+print('''
+Just because something
+thinks differently from you,
+dose that mean it\'s not thinking ?  
+''')
+
+print('''
+\\\t\t/
+\tx
+/\t\t\\
+''')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 500-exercises-Python
+This repo create for my learning and doing 500 exercises from
+Ref: หนังสือตะลุยโจทย์ Python 500 ข้อ
+https://www.facebook.com/tautologyai/posts/3062280547227159/
+
